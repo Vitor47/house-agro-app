487ea3636b9ec5fe0516db1752148ea11e96225e
Vitor Miolo <vitor.miolo@gestaods.com.br>
2024-08-14 21:21:55 -0300

    init

---
+++ nativescript.config.ts
@@ -0,0 +1,11 @@
+import { NativeScriptConfig } from '@nativescript/core';
+
+export default {
+  id: 'org.nativescript.houseagro',
+  appPath: 'src',
+  appResourcesPath: 'App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none'
+  }
+} as NativeScriptConfig;
