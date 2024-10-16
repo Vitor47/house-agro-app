d0d6d97120b3c5337cbab2d2d637316e02e493a0
Vitor Miolo <vitor.miolo@gestaods.com.br>
2024-10-16 19:20:38 -0300

    feat: add mapa com geolocalização atual

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MultiResult, request } from "@nativescript-community/perms";
 import {
-  ReactiveFormsModule
-} from "@angular/forms";
-
-import {
+  ActionBarComponent,
   NativeScriptFormsModule,
   NativeScriptRouterModule,
 } from "@nativescript/angular";
-
+import { CoreTypes } from "@nativescript/core";
+import * as geolocation from "@nativescript/geolocation";
+import {
+  CameraUpdate,
+  GoogleMap,
+  MapReadyEvent,
+} from "@nativescript/google-maps";
+import { GoogleMapsModule } from "@nativescript/google-maps/angular";
 
 @Component({
   selector: "ns-home",
@@ -18,7 +24,41 @@ import {
     NativeScriptRouterModule,
     ReactiveFormsModule,
     NativeScriptFormsModule,
+    ActionBarComponent,
+    GoogleMapsModule,
   ],
   schemas: [NO_ERRORS_SCHEMA],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  async onReady(event: MapReadyEvent) {
+    const map: GoogleMap = event.map;
+
+    const response = (await request("location", {
+      type: "always",
+    })) as unknown as MultiResult;
+    if (response["android.permission.ACCESS_COARSE_LOCATION"] == "authorized") {
+      map.myLocationEnabled = true;
+      map.uiSettings.myLocationButtonEnabled = true;
+
+      geolocation.enableLocationRequest().then(() => {
+        geolocation
+          .getCurrentLocation({
+            desiredAccuracy: CoreTypes.Accuracy.high,
+            maximumAge: 5000,
+            timeout: 20000,
+          })
+          .then((currentLocation) => {
+            map.animateCamera(
+              CameraUpdate.fromCoordinate(
+                {
+                  lat: currentLocation.latitude,
+                  lng: currentLocation.longitude,
+                },
+                40
+              )
+            );
+          });
+      });
+    }
+  }
+}
