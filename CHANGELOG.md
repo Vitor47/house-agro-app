231ed1f72ba4a78c75c1b30155f1dd54270b2df5
Vitor Miolo <vitor.miolo@gestaods.com.br>
2024-10-16 20:20:35 -0300

    feat: add animais fake no mapa

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,16 +18,3 @@
 
 * add filtro de busca por nome dos jogadores ([29c9668](https://github.com/Vitor47/house-agro-app/commit/29c966847f32c2cc97b293fe73031cb8ceb0da08))
 * layout de login ([625e726](https://github.com/Vitor47/house-agro-app/commit/625e72624f9308501853590cac5596381bdff446))
-
-
-
-# 1.0.0 (2024-10-02)
-
-
-### Features
-
-* add filtro de busca por nome dos jogadores ([29c9668](https://github.com/Vitor47/house-agro-app/commit/29c966847f32c2cc97b293fe73031cb8ceb0da08))
-* layout de login ([625e726](https://github.com/Vitor47/house-agro-app/commit/625e72624f9308501853590cac5596381bdff446))
-
-
-
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,13 @@ import {
   NativeScriptFormsModule,
   NativeScriptRouterModule,
 } from "@nativescript/angular";
-import { CoreTypes } from "@nativescript/core";
+import { CoreTypes, ImageSource } from "@nativescript/core";
 import * as geolocation from "@nativescript/geolocation";
 import {
   CameraUpdate,
   GoogleMap,
   MapReadyEvent,
+  MapView,
 } from "@nativescript/google-maps";
 import { GoogleMapsModule } from "@nativescript/google-maps/angular";
 
@@ -30,8 +31,19 @@ import { GoogleMapsModule } from "@nativescript/google-maps/angular";
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class HomeComponent {
+  mapView: MapView;
+  map: GoogleMap;
+
+  cattles = [];
+
+  public onMapReady(event) {
+    const mapView = event.object;
+    this.mapView = mapView;
+  }
+
   async onReady(event: MapReadyEvent) {
     const map: GoogleMap = event.map;
+    this.map = map;
 
     const response = (await request("location", {
       type: "always",
@@ -54,11 +66,78 @@ export class HomeComponent {
                   lat: currentLocation.latitude,
                   lng: currentLocation.longitude,
                 },
-                40
+                16
               )
             );
+
+            this.cattles = [
+              {
+                id: 1,
+                lat: currentLocation.latitude + 0.001,
+                lng: currentLocation.longitude + 0.001,
+              },
+              {
+                id: 2,
+                lat: currentLocation.latitude + 0.002,
+                lng: currentLocation.longitude - 0.001,
+              },
+              {
+                id: 3,
+                lat: currentLocation.latitude - 0.001,
+                lng: currentLocation.longitude + 0.002,
+              },
+              {
+                id: 4,
+                lat: currentLocation.latitude - 0.002,
+                lng: currentLocation.longitude - 0.002,
+              },
+              {
+                id: 5,
+                lat: currentLocation.latitude + 0.0015,
+                lng: currentLocation.longitude - 0.0015,
+              },
+            ];
+
+            let cattleIcon = ImageSource.fromFileSync("~/assets/cattle.webp");
+            cattleIcon = cattleIcon.resize(100);
+
+            this.cattles.forEach((cattle) => {
+              map.addMarker({
+                position: {
+                  lat: cattle.lat,
+                  lng: cattle.lng,
+                },
+                title: `Cattle ${cattle.id}`,
+                snippet: `Lat: ${cattle.lat}, Lng: ${cattle.lng}`,
+                icon: cattleIcon,
+              });
+            });
           });
       });
+    }
+  }
+
+  async onSearch(query: string) {
+    if (query) {
+      if (query) {
+        const cattleFound = this.cattles.filter((cattle) =>
+          `Cattle ${cattle.id}`.toLowerCase().includes(query.toLowerCase())
+        );
+
+        if (cattleFound.length) {
+          this.map.animateCamera(
+            CameraUpdate.fromCoordinates(
+              cattleFound.map((cattle) => ({
+                lat: cattle.lat,
+                lng: cattle.lng,
+              })),
+              16
+            )
+          );
+        } else {
+          console.log("Cattle não encontrado!");
+        }
+      }
     }
   }
 }
